5f88b9b44100c00c83b6d7a88a216c6a4322124d
Jonas Karlsson <96485472+kalsson@users.noreply.github.com>
2025-02-03 22:20:36 +0100

    Use SQL Server instead of in-memory database.
    Switched the database provider from in-memory to SQL Server for persistence. Updated `appsettings.json` with the connection string and the configuration in `Program.cs`. Adjusted project dependencies to include necessary SQL Server EF Core packages.

--- Todo.kalsson/TodoApi/Program.cs
+++ Todo.kalsson/TodoApi/Program.cs
@@ -2,7 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using TodoApi;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDbContext<TodoDb>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
